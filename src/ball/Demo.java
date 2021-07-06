513cd5c3bb1275572bc91be7a882f90b23a5e571
wsw <735475674@qq.com>
2021-07-06 10:25:33 +0800

    test

--- src/ball/Demo.java
+++ src/ball/Demo.java
@@ -2,5 +2,19 @@ package ball;
 
 import cn.tedu.util.App;
 public class Demo extends App{
+    public int a = 1;
 
+    public void a(){
+        System.out.println("AAAA");
+    }
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.b();
+    }
+
+    public void b(){
+        a();
+        System.out.println(this.a);
+    }
 }
